24bbb90b159c2349532ab70f369de5403a518da8
prawira <robbyprawira3@gmail.com>
2021-09-21 14:32:49 +0700

    learning nextjs

---
+++ pages/authors/me.js
@@ -0,0 +1,3 @@
+export default function me(){
+    return <h1>Hi, its me</h1>;
+}
---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from './components/layout'
+import utilStyles from './styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link'
+import Date from './components/date';
+
+export default function Home({ allPostData }){
+  console.log(allPostData)
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hoho hoho hohohohohohohohohohohohohohohohohohohohohohohohohoho</p>
+        <p>
+          (This is a sample website - you'll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>)
+        </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostData.map(({id, title, date}) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br/>
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}/>
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps(){
+  const allPostData = getSortedPostsData()
+  return{
+    props:{
+      allPostData
+    }
+  }
+  // let data;
+  // await fetch('https://jsonplaceholder.typicode.com/posts')
+  //       .then(response => response.json())
+  //       .then(json => {
+  //         data = json
+  //       })
+  // return {
+  //   props: {
+  //     allPostData: data
+  //   }
+  // }
+}
